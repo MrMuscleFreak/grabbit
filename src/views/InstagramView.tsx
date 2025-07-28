c117ab2e5792b4675186ecb75b1c2931a4e48f16
IPv3Address <vladi.klausar@gmail.com>
2025-07-28 17:53:36 +0200

    Update instagramView to new service changes.

--- src/views/InstagramView.tsx
+++ src/views/InstagramView.tsx
@@ -10,11 +10,11 @@ const InstagramView = () => {
       subtitle="Download videos, reels, and posts from Instagram."
       placeholder="https://www.instagram.com/p/..."
       fetchIpcChannel="get-instagram-info"
-      downloadIpcChannel="download-youtube-media" // Can be reused or changed
+      downloadIpcChannel="download-instagram-media"
       urlValidationRegex={
         /^(https?:\/\/)?(www\.)?instagram\.com\/(p|reel|reels|tv)\/.+$/
       }
-      urlValidationError="This link doesn’t appear to be from Instagram. If you're unsure, please use the 'Other' category."
+      urlValidationError="This link doesn't appear to be from Instagram. If you're unsure, please use the 'Other' category."
       mediaListTitle="Media in Post"
     />
   );
